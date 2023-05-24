89221dffc645910ff6a3aec51de3ab232cbbe6c6
alirezazandi <alirezazandi7800@gmail.com>
2023-05-24 12:41:17 +0330

    add task page

---
+++ src/pages/task/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+const Task = () => {
+  return (
+    <div>
+      <Button variant="contained">Hello</Button>
+    </div>
+  );
+};
+
+export default Task;
