00ece36ca4bb622c6ee157a8477bf7158e560bbb
“m.oraee1376” <m.oraee1376@gmail.com>
2023-05-24 15:19:50 +0330

    question

---
+++ src/pages/task/Question.tsx
@@ -0,0 +1,47 @@
+import {
+  Box,
+  Button,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+const data = {
+  title: "question 1",
+  description: "solve it!",
+  grade: null,
+  id: 1,
+  solved: false,
+};
+export const Question = () => {
+  return (
+    <Box m={4}>
+      <Box sx={{ marginBottom: "10px", marginTop: "10px" }}>
+        <Typography sx={{ ml: 2 }} variant="h4" gutterBottom>
+          {data.title}
+        </Typography>
+        <Typography sx={{ ml: 4 }} variant="h6" gutterBottom>
+          {data.description}
+        </Typography>
+      </Box>
+      <Stack direction="row" justifyContent="end">
+        <Select sx={{ margin: "10px" }} label="Language" value={0}>
+          <MenuItem value={0}>python</MenuItem>
+          <MenuItem value={1}>react js</MenuItem>
+          <MenuItem value={2}>java</MenuItem>
+        </Select>
+      </Stack>
+      <TextField fullWidth multiline label="Inter your code..." rows={10} />
+      <Stack direction="row" justifyContent="space-between">
+        <Button sx={{ m: "10px" }} variant="contained">
+          submit
+        </Button>
+        <Button sx={{ m: "10px" }} variant="outlined" color="secondary">
+          upload file
+        </Button>
+      </Stack>
+    </Box>
+  );
+};
