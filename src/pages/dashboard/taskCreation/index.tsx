3613c1b31174dd550cbe964dc8c28738b1e53316
“m.oraee1376” <m.oraee1376@gmail.com>
2023-05-24 21:10:57 +0330

    done

--- src/pages/dashboard/taskCreation/index.tsx
+++ src/pages/dashboard/taskCreation/index.tsx
@@ -1,7 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import Layout from "../layout";
+import { getQuestionsListRequest } from "../api";
+import { CustomTable } from "../../../components/table";
+import { CircularProgress, Stack } from "@mui/material";
 
 const TaskCreation = () => {
-  return <Layout title="Task Creation">Task Creation</Layout>;
+  const { data } = useQuery(["questionsList"], getQuestionsListRequest);
+
+  return (
+    <Layout title="Task Creation">
+      {data ? (
+        <CustomTable questionsList={data} isQuestionTable={false} />
+      ) : (
+        <Stack alignItems="center">
+          <CircularProgress />
+        </Stack>
+      )}
+    </Layout>
+  );
 };
 
 export default TaskCreation;
