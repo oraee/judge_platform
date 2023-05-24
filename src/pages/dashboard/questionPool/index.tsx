bba97bcb6a77bba5e39b91b0463f06486b35a125
alirezazandi <alirezazandi7800@gmail.com>
2023-05-24 17:42:15 +0330

    add dash

---
+++ src/pages/dashboard/questionPool/index.tsx
@@ -0,0 +1,7 @@
+import Layout from "../layout";
+
+const QuestionPool = () => {
+  return <Layout title="Question Pool">QuestionPool</Layout>;
+};
+
+export default QuestionPool;
---
+++ src/pages/dashboard/userRegistration/index.tsx
@@ -0,0 +1,7 @@
+import Layout from "../layout";
+
+const UserManagement = () => {
+  return <Layout title="User Management">User Management</Layout>;
+};
+
+export default UserManagement;
